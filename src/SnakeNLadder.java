4bbdbc13427d33529cb2de5381af530f2fd5562e
rishika7-hub <srishika970@gmail.com>
2020-11-11 21:29:55 +0530

    Main

---
+++ src/SnakeNLadder.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SnakeNLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
