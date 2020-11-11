f389c9be0c0b056d31600339012b18637b99af95
rishika7-hub <srishika970@gmail.com>
2020-11-12 00:03:11 +0530

    UC3 added

--- src/SnakeNLadder.java
+++ src/SnakeNLadder.java
@@ -44,6 +44,61 @@ public class SnakeNLadder {
 		n=r.nextInt(7);
 		return (n==0?1:n);
 	}
+	public void startGame1()
+	{
+		int player1 =0, player2=0;
+		int currentPlayer=-1;
+		Scanner s = new Scanner(System.in);
+		String str;
+		int diceValue =0;
+		do
+		{
+			System.out.println(currentPlayer==-1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
+			System.out.println("Press r to roll Dice");
+			str = s.next();
+			diceValue = rollDice();
+			
+			
+			if(currentPlayer == -1)
+			{
+				player1 = calculatePlayerValue(player1,diceValue);
+				System.out.println("First Player :: " + player1);
+				System.out.println("Second Player :: " + player2);
+				System.out.println("------------------");
+				if(isWin(player1))
+				{
+					System.out.println("First player wins");
+					return;
+				}
+			}
+			else
+			{
+				player2 = calculatePlayerValue(player2,diceValue);
+				System.out.println("First Player :: " + player1);
+				System.out.println("Second Player :: " + player2);
+				System.out.println("------------------");
+				if(isWin(player2))
+				{
+					System.out.println("Second player wins");
+					return;
+				}
+			}
+			
+			currentPlayer= -currentPlayer;
+			
+			
+			
+		}while("r".equals(str));
+	}
+	private boolean isWin(int player1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	private int calculatePlayerValue(int player1, int diceValue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 	
 
 }
